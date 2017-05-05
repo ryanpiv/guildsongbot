ceffcaba4eb21a98b5d9f46037f65eb9fdbe136b
Ryan <ryanpiveteau@gmail.com>
2017-05-05 17:49:48 -0400

    straw poll submit working, need to add vote options to index, countdown timer and begin sql history tracking

--- strawpollsubmit.php
+++ strawpollsubmit.php
@@ -2,23 +2,29 @@
 
 $url = 'https://strawpoll.me/api/v2/polls';
 
-$options = Array();
-foreach ($_POST as $key => $value) {
-	array_push($options, $value);
-}
-print_r($options);
+$votes = $_POST['options'];
 
-$headers[] = $options;
-$headers[] = "'multi': false";
+$options = array(
+	'title' => $_POST['title'],
+	'options' => json_decode($votes),
+);
+
+$options = json_encode($options);
+
+$headers = array(
+	"Content-Type: application/json",
+);
 
 $ch = curl_init();
 curl_setopt_array($ch, array(
 	CURLOPT_URL => $url,
-	CURLOPT_HTTPHEADER => array($headers),
 	CURLOPT_RETURNTRANSFER => 1,
 	CURLOPT_FOLLOWLOCATION => 1,
 	CURLOPT_VERBOSE => 1,
 	CURLOPT_SSL_VERIFYPEER => 0,
+	CURLOPT_POST => 1,
+	CURLOPT_POSTFIELDS => $options,
+	CURLOPT_HTTPHEADER => $headers,
 ));
 $response = curl_exec($ch);
 
