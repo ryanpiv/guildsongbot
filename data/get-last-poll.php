b3f4ba451ea36deb79210107fe64c633dd73383d
Ryan <ryanpiveteau@gmail.com>
2017-05-24 11:16:53 -0400

    website

---
+++ data/get-last-poll.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+$username = "disappointedsong";
+$password = "Ry7T0qQ~10R-";
+$hostname = "mysql4.gear.host";
+$db = "disappointedsong";
+$con = mysqli_connect($hostname, $username, $password, $db);
+
+//$index = $_GET['index'];
+
+//query to get next game in list
+$sql = "SELECT * FROM polls order by dateEnd desc limit 1";
+//LIMIT " . $index . ", 1";
+$result = $con->query($sql);
+
+try {
+	if (!$result) {
+		print_r("Error: " . $sql . "<br>" . $con->error);
+	} else {
+		$json = mysqli_fetch_all($result, MYSQLI_ASSOC);
+		echo json_encode($json);
+	}
+} catch (Exception $e) {
+	die($e);
+}
+
+?>
