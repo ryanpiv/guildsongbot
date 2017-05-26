133d5f5175e6735d4ba100ddb22694f3a148c0f4
Ryan <ryanpiveteau@gmail.com>
2017-05-26 12:55:20 -0400

    invasions, moment js
    to do: time zone date formatting output and storage

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,12 +1,16 @@
+//discord bot link: https://discordapp.com/developers/applications/me/316216046107361281
+
 var Discordie = require('discordie');
 var mysql = require('mysql');
 var request = require('request');
 var schedule = require('node-schedule');
+var moment = require('moment');
 
 const Events = Discordie.Events;
 const client = new Discordie();
 
-var songchannel = '300448798365450240';
+//var songchannel = '300448798365450240'; //real channel
+var songchannel = '266749722692288512'; //test channel
 
 var job_WinnerTracker;
 //executes function to record winning poll at the end of 7 days
@@ -19,6 +23,10 @@ Date.prototype.addDays = function(days) {
     dat.setDate(dat.getDate() + days);
     return dat;
 }
+Date.prototype.makeGMT = function(date) {
+    date = new Date(date.toGMTString());
+    return date;
+}
 
 var connection = mysql.createConnection({
     host: 'mysql4.gear.host',
@@ -27,9 +35,12 @@ var connection = mysql.createConnection({
     database: 'disappointedsong'
 });
 
+/*var aDate = new Date();
+console.log(moment(aDate).format('dddd, MMMM Do YYYY') + ' at ' + moment(aDate).utcOffset(0).format('h:mm:ss a'));*/
+
 client.connect({
-    //token: 'MzAwODEwOTE1NTg0OTMzODg4.C-0eXA.B6uha44_ewJlbJ4cTHfbOtFy2TQ'
-    token: 'MzAwODEwOTE1NTg0OTMzODg4.DAZIDw.RAsvejCdsKL986Vc6MdJEvpHV5c'
+    token: 'MzE2MjE2MDQ2MTA3MzYxMjgx.DAjXfQ.9UJJewQgiPFgYne--eF2SaL33OE' //test channel
+        //token: 'MzAwODEwOTE1NTg0OTMzODg4.DAZIDw.RAsvejCdsKL986Vc6MdJEvpHV5c' //real channel
 });
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
@@ -52,6 +63,30 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
             case '!clearmessages':
                 console.log('Clearing last 50 messages...');
                 clearMessages(50);
+                break;
+            case '!previouswinner':
+                sendDiscordMessage(songchannel, 'Feature coming soon!');
+                break;
+            case '!invasion est':
+                getCurrentInvasion(require('./invasions_est.json'), 240);
+                break;
+            case '!invasion cst':
+                getCurrentInvasion(require('./invasions_est.json'), 300);
+                break;
+            case '!invasion mdt':
+                getCurrentInvasion(require('./invasions_est.json'), 360);
+                break;
+            case '!invasion pst':
+                getCurrentInvasion(require('./invasions_est.json'), 400);
+                break;
+            case '!invasion gmt':
+                getCurrentInvasion(require('./invasions_est.json'), 0);
+                break;
+            case '!invasion':
+                getCurrentInvasion(require('./invasions_est.json'), 0);
+                break;
+            case '!commands':
+                sendDiscordMessage(songchannel, 'You can say: !smaktat, !collectVotes, !previousWinner, or !invasion <timezone> (EST, CST, MST, PST, GMT)');
                 break;
         }
     }
@@ -139,7 +174,8 @@ function fetchMessages(object) {
     //console.log(client.Messages.forChannel('300448798365450240'));
     //console.log(client);
     //bot.Channels.get('channelid').fetchMessages() // you can also use a raw channel object.
-    var options = ['https://www.youtube.com/watch?v=nxZORz9zx6w'];
+    //var options = ['https://www.youtube.com/watch?v=nxZORz9zx6w']; //testing
+    var options = [];
     var members = [];
     client.Channels.get(songchannel).fetchMessages().then(() => {
         var messages = client.Channels.get(songchannel).messages;
@@ -224,27 +260,27 @@ function winner() {
             if (!error && response.statusCode == 200) {
                 console.log(response.body);
                 var responseBody = JSON.parse(response.body);
-                for(var i = 0; i < responseBody.votes.length; i++){
-                	var index = 0;
-                	var maxVotes = 0;
-                	if(parseInt(responseBody.votes[i]) > maxVotes){
-                		maxVotes = responseBody.votes[i];
-                		index = i;
-                	}
+                for (var i = 0; i < responseBody.votes.length; i++) {
+                    var index = 0;
+                    var maxVotes = 0;
+                    if (parseInt(responseBody.votes[i]) > maxVotes) {
+                        maxVotes = responseBody.votes[i];
+                        index = i;
+                    }
                 }
-               	connection.query("UPDATE polls set winner= '" + responseBody.options[index] + "' where strawpollid=" + spid, function(error, results, fields) {
-               		if(error){
-               			sendDiscordMessage(songchannel, 'An error occurred while updating the Strawpoll winner: ' + error + ' @smaktat#1528');
-               		} else {
-               			sendDiscordMessage(songchannel, 'The voting period has ended! The winner is... : ' + responseBody.options[index]);
-               			sendDiscordMessage(songchannel, 'This song will rule for 4 weeks.  The next poll will begin in 3 weeks.  You can start posting submissions now!');
-               			var newPollDate = new Date();
-               			newPollDate = newPollDate.addDays(21);
-               			job_PollCreater = schedule.scheduleJob(newPollDate, function() {
-                			getActivePoll();
-                		});
-               		}
-               	});
+                connection.query("UPDATE polls set winner= '" + responseBody.options[index] + "' where strawpollid=" + spid, function(error, results, fields) {
+                    if (error) {
+                        sendDiscordMessage(songchannel, 'An error occurred while updating the Strawpoll winner: ' + error + ' @smaktat#1528');
+                    } else {
+                        sendDiscordMessage(songchannel, 'The voting period has ended! The winner is... : ' + responseBody.options[index]);
+                        sendDiscordMessage(songchannel, 'This song will rule for 4 weeks.  The next poll will begin in 3 weeks.  You can start posting submissions now!');
+                        var newPollDate = new Date();
+                        newPollDate = newPollDate.addDays(21);
+                        job_PollCreater = schedule.scheduleJob(newPollDate, function() {
+                            getActivePoll();
+                        });
+                    }
+                });
             } else {
                 sendDiscordMessage(songchannel, 'An error occurred while getting the Strawpoll: ' + ' @smaktat#1528');
             }
@@ -264,4 +300,64 @@ function clearMessages(amount) {
     }).catch((err) => {
         sendDiscordMessage(songchannel, err);
     });
+}
+
+function getCurrentInvasion(invasions, tz) {
+    console.log('finding invasions');
+    var currDate = new Date();
+    for (var i = 0; i < invasions.length; i++) {
+        var obj = invasions[i];
+        var start = new Date(obj['FIELD1']);
+        var end = new Date(obj['FIELD2']);
+
+        start.setYear('2017');
+        end.setYear('2017');
+
+        if (!tz) {
+            tz = 0;
+        }
+
+        //console.log(moment(aDate).format('dddd, MMMM Do YYYY') + ' at ' + moment(aDate).utcOffset(0).format('h:mm:ss a'));
+
+        //currDate.getTimezoneOffset(); //240 for EST
+
+        //console.log('currDate: ' + currDate + ', time: ' + currDate.getTime());
+        //console.log('start: ' + start + ', end: ' + end);
+
+        if (currDate.getTime() <= end.getTime()) {
+            //invasion hasn't ended
+            var timeDiff = end.getTime() - currDate.getTime();
+            var timeLeft_Hours = Math.floor(timeDiff / 3600000);
+            var timeLeft_Minutes = Math.floor(timeDiff / 60000);
+            var minutes_factor = Math.floor(timeLeft_Minutes / 60);
+            timeLeft_Minutes -= 60 * minutes_factor;
+            if (currDate.getTime() >= start.getTime()) {
+                //invasion is currently happening
+                sendDiscordMessage(songchannel, 'An invasion is happening now!  It will end at ' + moment(end).utcOffset(tz).format('h:mm a z') + '.  There are ' + timeLeft_Hours + ' hour(s) and ' + timeLeft_Minutes + ' minute(s) left before it ends.');
+            } else {
+                //this will be the next invasion
+                sendDiscordMessage(songchannel, 'The next invasion will begin on ' + start.toLocaleDateString() + ' at ' + moment(start).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z') + '.  There are ' + timeLeft_Hours + ' hours and ' + timeLeft_Minutes + ' minutes until this invasion will begin.');
+            }
+            //output next 3 invasion times here
+            var start_1, start_2, start_3, end_1, end_2, end_3;
+            if (invasions[i + 1]) {
+                obj = invasions[i + 1];
+                start_1 = new Date(obj['FIELD1']);
+                end_1 = new Date(obj['FIELD2']);
+            }
+            if (invasions[i + 2]) {
+                obj = invasions[i + 2];
+                start_2 = new Date(obj['FIELD1']);
+                end_2 = new Date(obj['FIELD2']);
+            }
+            if (invasions[i + 3]) {
+                obj = invasions[i + 3];
+                start_3 = new Date(obj['FIELD1']);
+                end_3 = new Date(obj['FIELD2']);
+            }
+            sendDiscordMessage(songchannel, 'Here are the next 3 invasions.\nBegin: ' + moment(start_1).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z') + ' | ' + ' End: ' + moment(end_1).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z') + '\nBegin: ' + moment(start_2).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z') + ' | ' + ' End: ' + moment(end_2).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z') + '\nBegin: ' + moment(start_3).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z') + ' | ' + ' End: ' + moment(end_3).utcOffset(tz).format('dddd, MMMM Do YYYY, h:mm a z'));
+
+            break;
+        }
+    }
 }
